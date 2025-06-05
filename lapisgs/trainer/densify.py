98ea6c1a18b0ead065e0c955237234a0ed83a668
yindaheng98 <yindaheng98@gmail.com>
2025-06-04 20:59:02 -0700

    combinations

--- lapisgs/trainer/densify.py
+++ lapisgs/trainer/densify.py
@@ -1,6 +1,7 @@
+from typing import Callable
 from gaussian_splatting import GaussianModel
 from gaussian_splatting.trainer import DensificationTrainer
-from gaussian_splatting.trainer.densifier import AbstractDensifier
+from gaussian_splatting.trainer.densifier import AbstractDensifier, SplitCloneDensifier
 
 
 class PartialDensificationTrainer(DensificationTrainer):
@@ -27,3 +28,34 @@ class PartialDensificationTrainer(DensificationTrainer):
     def remove_points(self, rm_mask):
         rm_mask[:self.size_fixed_gs] = False
         return super().remove_points(rm_mask)
+
+
+def SplitClonePartialDensifierTrainerWrapper(
+        noargs_base_densifier_constructor: Callable[[GaussianModel, float], AbstractDensifier],
+        model: GaussianModel,
+        scene_extent: float,
+        *args,
+        densify_from_iter=500,
+        densify_until_iter=15000,
+        densify_interval=100,
+        densify_grad_threshold=0.0002,
+        densify_percent_dense=0.01,
+        densify_percent_too_big=0.8,
+        **kwargs):
+    # https://github.com/yindaheng98/gaussian-splatting/blob/56576b647d9c5bd05300f5640cd03a8c75a760bc/gaussian_splatting/trainer/densifier/densifier.py#L149
+    densifier = noargs_base_densifier_constructor(model, scene_extent)
+    densifier = SplitCloneDensifier(
+        densifier,
+        scene_extent,
+        densify_from_iter=densify_from_iter,
+        densify_until_iter=densify_until_iter,
+        densify_interval=densify_interval,
+        densify_grad_threshold=densify_grad_threshold,
+        densify_percent_dense=densify_percent_dense,
+        densify_percent_too_big=densify_percent_too_big
+    )
+    return PartialDensificationTrainer(
+        model, scene_extent,
+        densifier,
+        *args, **kwargs
+    )
