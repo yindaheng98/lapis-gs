023c9ec4677ba8dee8f045ca91fb387584783a8b
yindaheng98 <yindaheng98@gmail.com>
2025-06-05 15:19:31 -0700

    gather common func

---
+++ lapisgs/prepare.py
@@ -0,0 +1,54 @@
+from gaussian_splatting import GaussianModel
+from gaussian_splatting.dataset import CameraDataset
+from gaussian_splatting.trainer import AbstractTrainer
+from gaussian_splatting.trainer.extensions import ScaleRegularizeTrainerWrapper
+from lapisgs.dataset import RescaleJSONCameraDataset, RescaleColmapCameraDataset, RescaleTrainableCameraDataset, RescaleColmapTrainableCameraDataset
+from lapisgs.trainer import Trainer, CameraTrainer, DepthTrainer, DepthCameraTrainer
+
+
+def prepare_dataset(source: str, device: str, trainable_camera: bool = False, load_camera: str = None, load_depth=False, rescale_factor=1.0) -> CameraDataset:
+    if trainable_camera:
+        dataset = (RescaleJSONCameraDataset(load_camera, load_depth=load_depth, rescale_factor=rescale_factor)
+                   if load_camera else RescaleColmapCameraDataset(source, load_depth=load_depth, rescale_factor=rescale_factor)).to(device)
+    else:
+        dataset = (RescaleTrainableCameraDataset.from_json(load_camera, load_depth=load_depth, rescale_factor=rescale_factor)
+                   if load_camera else RescaleColmapTrainableCameraDataset(source, load_depth=load_depth, rescale_factor=rescale_factor)).to(device)
+    return dataset
+
+
+modes = {
+    "base": DepthTrainer,
+    "camera": DepthCameraTrainer,
+    "nodepth-base": Trainer,
+    "nodepth-camera": CameraTrainer,
+}
+
+
+def prepare_trainer(gaussians: GaussianModel, dataset: CameraDataset, mode: str, trainable_camera: bool = False, load_ply: str = None, with_scale_reg=False, configs={}) -> AbstractTrainer:
+    if not load_ply:
+        from gaussian_splatting.train import prepare_trainer as legacy_prepare_trainer
+        modemap = {
+            "base": "densify",
+            "camera": "camera-densify",
+            "nodepth-base": "nodepth-densify",
+            "nodepth-camera": "nodepth-camera-densify",
+        }
+        return legacy_prepare_trainer(
+            gaussians=gaussians, dataset=dataset, mode=modemap[mode], trainable_camera=trainable_camera, load_ply=load_ply, with_scale_reg=with_scale_reg, configs=configs)
+    constructor = modes[mode]
+    if with_scale_reg:
+        constructor = lambda *args, **kwargs: ScaleRegularizeTrainerWrapper(modes[mode], *args, **kwargs)
+    if trainable_camera:
+        trainer = constructor(
+            gaussians,
+            scene_extent=dataset.scene_extent(),
+            dataset=dataset,
+            **configs
+        )
+    else:
+        trainer = constructor(
+            gaussians,
+            scene_extent=dataset.scene_extent(),
+            **configs
+        )
+    return trainer
--- lapisgs/train.py
+++ lapisgs/train.py
@@ -3,69 +3,16 @@ from typing import Tuple
 import torch
 from gaussian_splatting import GaussianModel
 from gaussian_splatting.dataset import CameraDataset
-from gaussian_splatting.trainer import *
-from gaussian_splatting.trainer.extensions import ScaleRegularizeTrainerWrapper
-from gaussian_splatting.train import save_cfg_args, training, basemodes, shliftmodes, prepare_gaussians
-from lapisgs.dataset import RescaleJSONCameraDataset, RescaleColmapCameraDataset, RescaleTrainableCameraDataset, RescaleColmapTrainableCameraDataset
-from lapisgs.trainer import Trainer, CameraTrainer, DepthTrainer, DepthCameraTrainer
-
-modes = {
-    "base": DepthTrainer,
-    "camera": DepthCameraTrainer,
-    "nodepth-base": Trainer,
-    "nodepth-camera": CameraTrainer,
-}
+from gaussian_splatting.trainer import AbstractTrainer
+from gaussian_splatting.prepare import prepare_gaussians
+from gaussian_splatting.train import save_cfg_args, training
+from lapisgs.prepare import modes, prepare_dataset, prepare_trainer
 
 
-def prepare_dataset(source: str, device: str, mode: str, load_camera: str = None, load_depth=False, rescale_factor=1.0):
-    match mode:
-        case "base" | "nodepth":
-            dataset = (RescaleJSONCameraDataset(load_camera, load_depth=load_depth, rescale_factor=rescale_factor) if load_camera else RescaleColmapCameraDataset(source, load_depth=load_depth, rescale_factor=rescale_factor)).to(device)
-        case "camera" | "nodepth-camera":
-            dataset = (RescaleTrainableCameraDataset.from_json(load_camera, load_depth=load_depth, rescale_factor=rescale_factor)
-                       if load_camera else RescaleColmapTrainableCameraDataset(source, load_depth=load_depth, rescale_factor=rescale_factor)).to(device)
-        case _:
-            raise ValueError(f"Unknown mode: {mode}")
-    return dataset
-
-
-def prepare_trainer(gaussians: GaussianModel, dataset: CameraDataset, mode: str, load_ply: str = None, with_scale_reg=False, configs={}) -> AbstractTrainer:
-    if not load_ply:
-        from gaussian_splatting.train import prepare_trainer as legacy_prepare_trainer
-        modemap = {
-            "base": "densify",
-            "camera": "camera-densify",
-            "nodepth-base": "nodepth-densify",
-            "nodepth-camera": "nodepth-camera-densify",
-        }
-        return legacy_prepare_trainer(
-            gaussians=gaussians, dataset=dataset, mode=modemap[mode], load_ply=load_ply, with_scale_reg=with_scale_reg, configs=configs)
-    constructor = modes[mode]
-    if with_scale_reg:
-        constructor = lambda *args, **kwargs: ScaleRegularizeTrainerWrapper(modes[mode], *args, **kwargs)
-    match mode:
-        case "base" | "nodepth":
-            trainer = constructor(
-                gaussians,
-                scene_extent=dataset.scene_extent(),
-                **configs
-            )
-        case "camera" | "nodepth-camera":
-            trainer = constructor(
-                gaussians,
-                scene_extent=dataset.scene_extent(),
-                dataset=dataset,
-                **configs
-            )
-        case _:
-            raise ValueError(f"Unknown mode: {mode}")
-    return trainer
-
-
-def prepare_training(sh_degree: int, source: str, device: str, mode: str, load_ply: str = None, load_camera: str = None, load_depth=False, rescale_factor=1.0, with_scale_reg=False, configs={}) -> Tuple[CameraDataset, GaussianModel, AbstractTrainer]:
-    dataset = prepare_dataset(source=source, device=device, mode=mode, load_camera=load_camera, load_depth=load_depth, rescale_factor=rescale_factor)
-    gaussians = prepare_gaussians(sh_degree=sh_degree, source=source, device=device, mode=mode, load_ply=load_ply)
-    trainer = prepare_trainer(gaussians=gaussians, dataset=dataset, mode=mode, load_ply=load_ply, with_scale_reg=with_scale_reg, configs=configs)
+def prepare_training(sh_degree: int, source: str, device: str, mode: str, trainable_camera: bool = False, load_ply: str = None, load_camera: str = None, load_depth=False, rescale_factor=1.0, with_scale_reg=False, configs={}) -> Tuple[CameraDataset, GaussianModel, AbstractTrainer]:
+    dataset = prepare_dataset(source=source, device=device, trainable_camera=trainable_camera, load_camera=load_camera, load_depth=load_depth, rescale_factor=rescale_factor)
+    gaussians = prepare_gaussians(sh_degree=sh_degree, source=source, device=device, trainable_camera=trainable_camera, load_ply=load_ply)
+    trainer = prepare_trainer(gaussians=gaussians, dataset=dataset, mode=mode, trainable_camera=trainable_camera, load_ply=load_ply, with_scale_reg=with_scale_reg, configs=configs)
     return dataset, gaussians, trainer
 
 
@@ -81,7 +28,7 @@ if __name__ == "__main__":
     parser.add_argument("--no_depth_data", action="store_true")
     parser.add_argument("--rescale_factor", default=1.0, type=float)
     parser.add_argument("--with_scale_reg", action="store_true")
-    parser.add_argument("--mode", choices=sorted(list(set(list(modes.keys()) + list(basemodes.keys()) + list(shliftmodes.keys())))), default="base")
+    parser.add_argument("--mode", choices=sorted(list(modes.keys())), default="base")
     parser.add_argument("--save_iterations", nargs="+", type=int, default=[7000, 30000])
     parser.add_argument("--device", default="cuda", type=str)
     parser.add_argument("-o", "--option", default=[], action='append', type=str)
@@ -91,7 +38,7 @@ if __name__ == "__main__":
 
     configs = {o.split("=", 1)[0]: eval(o.split("=", 1)[1]) for o in args.option}
     dataset, gaussians, trainer = prepare_training(
-        sh_degree=args.sh_degree, source=args.source, device=args.device, mode=args.mode,
+        sh_degree=args.sh_degree, source=args.source, device=args.device, mode=args.mode, trainable_camera="camera" in args.mode,
         load_ply=args.foundation_gs_path, load_camera=args.load_camera, load_depth=not args.no_depth_data, rescale_factor=args.rescale_factor, with_scale_reg=args.with_scale_reg, configs=configs)
     dataset.save_cameras(os.path.join(args.destination, "cameras.json"))
     torch.cuda.empty_cache()
