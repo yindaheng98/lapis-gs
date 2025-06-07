b7b81fc1a7a562ac438357b6160e49e02266e5ef
yindaheng98 <yindaheng98@gmail.com>
2025-06-07 15:01:28 -0700

    train full pipeline in shell

---
+++ train_full_pipeline.sh
@@ -0,0 +1,31 @@
+GLOBALARGS="-olambda_dssim=0.8"
+pipeline() {
+    python -m lapisgs.train_reduced \
+        -s data/$1 -d output/$1/8x \
+        --rescale_factor 0.125 -i $2 \
+        --mode shculling \
+        $GLOBALARGS
+    python -m lapisgs.train_reduced \
+        -s data/$1 -d output/$1/4x \
+        --rescale_factor 0.25 -i $2 \
+        --mode camera-shculling \
+        -l output/truck/8x/point_cloud/iteration_$2/point_cloud.ply \
+        --load_camera output/truck/8x/cameras.json \
+        $GLOBALARGS
+    python -m lapisgs.train_reduced \
+        -s data/$1 -d output/$1/2x \
+        --rescale_factor 0.5 -i $2 \
+        --mode camera-shculling \
+        -l output/truck/4x/point_cloud/iteration_$2/point_cloud.ply \
+        --load_camera output/truck/4x/cameras.json \
+        $GLOBALARGS
+    python -m lapisgs.train_reduced \
+        -s data/$1 -d output/$1/1x \
+        --rescale_factor 1.0 -i $2 \
+        --mode camera-shculling \
+        -l output/truck/2x/point_cloud/iteration_$2/point_cloud.ply \
+        --load_camera output/truck/2x/cameras.json \
+        $GLOBALARGS
+}
+
+pipeline truck 30000
