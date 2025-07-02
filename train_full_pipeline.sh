49984149d91c46a5f16915778d99568c3b28341a
yindaheng98 <yindaheng98@gmail.com>
2025-07-01 21:59:59 -0700

    Do not optimize camera but use camera rendering

--- train_full_pipeline.sh
+++ train_full_pipeline.sh
@@ -1,13 +1,18 @@
 ITERS=30000
-GLOBALARGS="-olambda_dssim=0.8"
-FUNDATIONMODE="camera-shculling"
+GLOBALARGS="
+    -olambda_dssim=0.8 \
+    -ocamera_rotation_lr_init=0 \
+    -ocamera_rotation_lr_init=0 \
+    -ocamera_position_lr_init=0 \
+    -ocamera_position_lr_final=0 \
+    -ocamera_exposure_lr_init=0 \
+    -ocamera_exposure_lr_final=0" # Do not optimize camera (load from reduced-3dgs, already trained)
+FUNDATIONMODE="camera-shculling" # But use camera rendering
 FUNDATIONARGS="
-    -olambda_dssim=0.8
     -omercy_type='redundancy_opacity_opacity' \
     -oimportance_prune_interval=100"
 ENHANCEMODE="camera-shculling"
 ENHANCEARGS="
-    -olambda_dssim=0.8
     -omercy_type='redundancy_opacity_opacity' \
     -oimportance_prune_interval=100 \
     -odensify_interval=500 \
@@ -16,39 +21,42 @@ BASRLINEITERS=30000
 BASELINEMODE="camera-densify-prune-shculling"
 BASELINEARGS="-omercy_type='redundancy_opacity_opacity'"
 pipeline() {
+    # Train the baseline model, regular reduced-3dgs
+    python -m reduced_3dgs.train \
+        -s data/$1 -d output/$1 \
+        -i $BASRLINEITERS \
+        --mode $BASELINEMODE \
+        $BASELINEARGS
+    # Train the fundation layer (8x), use the trained camera and scene from reduced-3dgs
     python -m lapisgs.train_reduced \
         -s data/$1 -d output/$1/8x \
         --rescale_factor 0.125 -i $ITERS \
         --mode $FUNDATIONMODE \
+        -l output/$1/point_cloud/iteration_$ITERS/point_cloud.ply \
+        --load_camera output/$1/cameras.json \
         $GLOBALARGS $FUNDATIONARGS
+    # Train the next layer (4x), use the trained camera from reduced-3dgs and scene from 8x
     python -m lapisgs.train_reduced \
         -s data/$1 -d output/$1/4x \
         --rescale_factor 0.25 -i $ITERS \
         --mode $ENHANCEMODE \
         -l output/$1/8x/point_cloud/iteration_$ITERS/point_cloud.ply \
-        --load_camera output/$1/8x/cameras.json \
+        --load_camera output/$1/cameras.json \
         $GLOBALARGS $ENHANCEARGS
     python -m lapisgs.train_reduced \
         -s data/$1 -d output/$1/2x \
         --rescale_factor 0.5 -i $ITERS \
         --mode $ENHANCEMODE \
         -l output/$1/4x/point_cloud/iteration_$ITERS/point_cloud.ply \
-        --load_camera output/$1/4x/cameras.json \
+        --load_camera output/$1/cameras.json \
         $GLOBALARGS $ENHANCEARGS
     python -m lapisgs.train_reduced \
         -s data/$1 -d output/$1/1x \
         --rescale_factor 1.0 -i $ITERS \
         --mode $ENHANCEMODE \
         -l output/$1/2x/point_cloud/iteration_$ITERS/point_cloud.ply \
-        --load_camera output/$1/2x/cameras.json \
+        --load_camera output/$1/cameras.json \
         $GLOBALARGS $ENHANCEARGS
-    python -m reduced_3dgs.train \
-        -s data/$1 -d output/$1 \
-        -i $BASRLINEITERS \
-        --mode $BASELINEMODE \
-        -l output/$1/1x/point_cloud/iteration_$ITERS/point_cloud.ply \
-        --load_camera output/$1/1x/cameras.json \
-        $BASELINEARGS
 }
 
 pipeline truck
