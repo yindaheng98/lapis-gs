b96219f60abbd6fb1aa7a8b55ffe0360689cccb9
yindaheng98 <yindaheng98@gmail.com>
2026-04-24 23:36:55 -0700

    upgrade

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ packages += ['lapisgs.trainer.extensions.' + package for package in find_package
 
 setup(
     name="lapisgs",
-    version='1.3.1',
+    version='1.3.2',
     author='yindaheng98',
     author_email='yindaheng98@gmail.com',
     url='https://github.com/yindaheng98/lapis-gs',
@@ -34,7 +34,7 @@ setup(
         'lapisgs': 'lapisgs',
     },
     install_requires=[
-        'gaussian-splatting >= 2.3.0',
+        'gaussian-splatting >= 2.3.8',
         'reduced-3dgs'
     ]
 )
